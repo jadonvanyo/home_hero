015faec6ce955b19e522524cf215dcb6fdfb9a61
jayman779 <jadon.vanyo@gmail.com>
2024-02-08 14:07:13 -0500

    added ability to parse though house cards from a Zillow page

--- homescraper/homescraper/spiders/homespider.py
+++ homescraper/homescraper/spiders/homespider.py
@@ -6,6 +6,24 @@ class HomespiderSpider(scrapy.Spider):
     allowed_domains = ["www.zillow.com"]
     start_urls = ["https://www.zillow.com/homedetails/11801-Franklin-Blvd-Lakewood-OH-44107/33503273_zpid/"]
 
+    # Overwrite any of the settings.py settings
+    custom_settings = {
+        # Close the spider after 2 items have been selected
+        'CLOSESPIDER_ITEMCOUNT': 2,
+    }
+
+    def parse(self, response):
+        """Navigate through each of the houses on a given page"""
+        
+        # Get all of the links for all the houses on the page
+        house_links = response.xpath('//a[contains(@data-test, "property-card-link")]/@href').getall()
+        
+        # Loop though all of the links to houses on the given zillow page
+        for link in house_links:
+            # Go into the home page and scrape the data
+            yield response.follow(link, callback=self.parse_zillow_house_page)
+            
+
     def parse_zillow_house_page(self, response):
         """Crawl and gather all of the information on a particular house's page"""
         
@@ -23,6 +41,3 @@ class HomespiderSpider(scrapy.Spider):
         
         yield home_item
         
-            
-    def parse(self, response):
-        pass
