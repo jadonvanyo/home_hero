f5f5498628cfc9f7bd28855c7d5df71c1a814d11
jayman779 <jadon.vanyo@gmail.com>
2024-02-09 20:30:04 -0500

    updated settings.py to include headers to get into the county office website

--- homescraper/homescraper/settings.py
+++ homescraper/homescraper/settings.py
@@ -43,6 +43,7 @@ DOWNLOAD_DELAY = 3
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
+    'authority': 'www.countyoffice.org',
     'pragma': 'no-cache',
     'cache-control': 'no-cache',
     'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
@@ -50,12 +51,12 @@ DEFAULT_REQUEST_HEADERS = {
     'sec-ch-ua-platform': '"macOS"',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site': 'none',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'sec-fetch-site': 'cross-site',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-user': '?1',
     'sec-fetch-dest': 'document',
-    'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,lt;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
 }
 
 # Change the headers to escape being blocked by a website
