3a4251e6a463b26a3243dd85da7628e00f21bec1
jayman779 <jadon.vanyo@gmail.com>
2024-02-11 00:51:29 -0500

    implemented taxspider.py, needs testing

--- homescraper/homescraper/settings.py
+++ homescraper/homescraper/settings.py
@@ -42,22 +42,22 @@ DOWNLOAD_DELAY = 3
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-    'authority': 'www.countyoffice.org',
-    'pragma': 'no-cache',
-    'cache-control': 'no-cache',
-    'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'sec-fetch-site': 'none',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'accept-language': 'en-US,en;q=0.9',
-}
+# DEFAULT_REQUEST_HEADERS = {
+#     'authority': 'www.countyoffice.org',
+#     'pragma': 'no-cache',
+#     'cache-control': 'no-cache',
+#     'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-ch-ua-platform': '"macOS"',
+#     'upgrade-insecure-requests': '1',
+#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+#     'sec-fetch-site': 'none',
+#     'sec-fetch-mode': 'navigate',
+#     'sec-fetch-user': '?1',
+#     'sec-fetch-dest': 'document',
+#     'accept-language': 'en-US,en;q=0.9',
+# }
 
 # Change the headers to escape being blocked by a website
 SCRAPEOPS_API_KEY = '4aac2734-0c08-4286-885f-91878b766e16'
--- homescraper/homescraper/spiders/taxspider.py
+++ homescraper/homescraper/spiders/taxspider.py
@@ -2,15 +2,37 @@ from homescraper.items import TaxItem
 import json
 import scrapy
 
-# TODO: Change the DEFAULT_REQUEST_HEADERS in settings
-
-##################### WARNING #########################
-# DO NOT RUN THIS SPIDER, IT IS NOT READY FOR USE
-
 class TaxspiderSpider(scrapy.Spider):
     name = "taxspider"
     allowed_domains = ["www.countyoffice.org"]
     start_urls = ["https://www.countyoffice.org/"]
+    
+        # Overwrite any of the settings.py settings for this particular spider \
+    custom_settings = {
+        
+        # Override the default request headers:
+        'DEFAULT_REQUEST_HEADERS': {
+            'authority': 'www.countyoffice.org',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-US,en;q=0.9',
+        }, 
+        
+        # Give a specific file and format to always save the data to
+        'FEEDS': {
+            'taxdata.json': {'format': 'json', 'overwrite': True}
+        },
+    }
 
     def parse(self, response):
         """Navigate to the page for each of the houses from homedata.json"""
@@ -27,16 +49,26 @@ class TaxspiderSpider(scrapy.Spider):
             value = "-".join(value[1:-1])
             tax_url = "https://www.countyoffice.org/" + value + "-property-records/"
             
-            # TODO: Navigate to one page, then navigate to the property details page
-            yield response.follow(tax_url, callback=self.parse_tax_page, meta={'address_number': address_number})
+            # Navigate to the street page with the address numbers
+            yield response.follow(tax_url, callback=self.parse_street_page, meta={'address_number': address_number})
         
+    def parse_street_page(self, response):
+        """Parse the tax page and navigate further based on address_number"""
+        
+        # Extract address_number from meta
+        address_number = response.meta.get('address_number')
+        
+        # Find the link for the specific house data
+        property_page_url = 'https://www.countyoffice.org/' + response.xpath(f'//ul/li/a[contains(@href, "{address_number}")]/@href').get()
+        
+        # Navigate to the property page to pull the required information
+        yield response.follow(property_page_url, callback=self.parse_property_page)
 
-    def parse_county_office_page(self, response):
-        """Crawl and gather the tax information for a give house"""
+    def parse_property_page(self, response):
+        """Crawl and gather the tax information for a given house"""
         
         home_item = TaxItem()
         home_item['url'] = response.url
-        # TODO: Correct the response for this item
-        home_item['tax'] = response.css('div.styles__AddressWrapper-sc-13x5vko-0.bCvhGr h1::text').getall()
+        home_item['tax'] = response.xpath('//table[contains(@id, "taxes")]/tbody/tr[1]/td[2]/text()').get()
         
         yield home_item
