0b30bcb13adea905295f2b32cfc4094be36bab08
jayman779 <jadon.vanyo@gmail.com>
2024-02-12 13:19:04 -0500

    successfully tested selenium and adjusted rentspider.py default headers

--- homescraper/homescraper/spiders/rentspider.py
+++ homescraper/homescraper/spiders/rentspider.py
@@ -6,5 +6,35 @@ class RentspiderSpider(scrapy.Spider):
     allowed_domains = ["www.zillow.com"]
     start_urls = ["https://www.zillow.com/rental-manager/price-my-rental/"]
 
+        # Overwrite any of the settings.py settings for this particular spider \
+    custom_settings = {
+        # Override the default request headers:
+        'DEFAULT_REQUEST_HEADERS': {
+            'authority': 'www.zillow.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,lt;q=0.7',
+        },
+        
+        # Give a specific file and format to always save the data to
+        'FEEDS': {
+            'homedata.json': {'format': 'json', 'overwrite': True}
+        },
+        
+        # Configure custom item pipelines
+        'ITEM_PIPELINES': {
+        }
+    }
+
     def parse(self, response):
         pass
--- homescraper/test.py
+++ homescraper/test.py
@@ -1,20 +1,11 @@
-import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
+driver = webdriver.Chrome()
 
-with open('homedata.json') as file:
-    data = json.load(file)
+driver.get("https://www.zillow.com/rental-manager/price-my-rental/results/12929-plover-st-lakewood-oh-44107/")
 
-for item in data:
-    value = item.get('url').split("/")[4].lower()
-    value = value.split("-")
-    address_number = value[0]
-    value = "-".join(value[1:-1])
-    new_url = "https://www.countyoffice.org/" + value + "-property-records/"
-        ## value = value.split("/")
-        ## value = value[4].lower()
-        # value = value.split("-", 1)
-        # value = value[1][::-1]
-        # value = value.split("-", 1)
-        # value = value[1][::-1]
-    print(address_number)
-    print(new_url)
+time.sleep(10)
+
+driver.quit()
