4b9ea48c241d9c9907b2f1eccd10dd59a2e10da2
jayman779 <jadon.vanyo@gmail.com>
2024-02-26 14:04:22 -0500

    removed the commented out request headers in defualt_request_headers since rotating headers is now effective

--- homescraper/homescraper/settings.py
+++ homescraper/homescraper/settings.py
@@ -47,17 +47,8 @@ DEFAULT_REQUEST_HEADERS = {
     'referer': 'http://www.google.com/',
     'pragma': 'no-cache',
     'cache-control': 'no-cache',
-    # 'sec-ch-ua': '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"', 
-    # 'sec-ch-ua-mobile': '?0',
-    # 'sec-ch-ua-platform': '"Windows"', # THIS NEEDS TO BE IN DOUBLE QUOTES
-    # 'upgrade-insecure-requests': '1',
-    # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
-    # 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    # 'sec-fetch-site': 'none',
     'sec-fetch-mode': 'navigate',
-    # 'sec-fetch-user': '?1',
     'sec-fetch-dest': 'document',
-    # 'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,lt;q=0.7',
 }
 
 # TODO: DELETE this
