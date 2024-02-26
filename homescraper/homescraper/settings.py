ee49feba7572c1254befc3b213085c0cc22f9c95
jayman779 <jadon.vanyo@gmail.com>
2024-02-26 12:58:30 -0500

    increased the maximum number of concurrent requests from 2 to 8 in settings.py

--- homescraper/homescraper/settings.py
+++ homescraper/homescraper/settings.py
@@ -25,7 +25,7 @@ NEWSPIDER_MODULE = "homescraper.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
