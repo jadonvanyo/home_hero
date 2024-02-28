7795c443a3a18072bd66b9227d72c62dbe984a48
jayman779 <jadon.vanyo@gmail.com>
2024-02-28 12:34:49 -0500

    added new unit tests to verify that the user has entered a valid api key

--- homescraper/unittests.py
+++ homescraper/unittests.py
@@ -6,7 +6,7 @@ class TestConfigFileRequiredValuesPresent(unittest.TestCase):
     def test_all_values_correct(self):
         """Test case where all config values are correct."""
         config = {
-            "scrapeops_api_key": "4aac8234-0r08-4286-345f-91878b766e57",
+            "scrapeops_api_key": "d875dca3-61b5-4126-9181-24e588fe58d3",
             "starturls": ["https://www.zillow.com/fort-lauderdale-fl/duplex/"],
             "down_payment_decimal": 0.12, 
             "closing_cost_buyer_decimal": 0.03,
@@ -23,6 +23,27 @@ class TestConfigFileRequiredValuesPresent(unittest.TestCase):
             "send_emails": False,
         }
         self.assertTrue(config_file_required_values_present(config))
+
+    def test_invalid_scrapeops_api_key(self):
+        """Test case where there is an invalid Scrapeops API key."""
+        config = {
+            "scrapeops_api_key": "a" * 36,
+            "starturls": ["https://www.zillow.com/fort-lauderdale-fl/duplex/"],
+            "down_payment_decimal": 0.12, 
+            "closing_cost_buyer_decimal": 0.03,
+            "closing_cost_seller_decimal": 0.08,
+            "expected_annual_growth": 0.02,
+            "interest_rate": 0.06,
+            "loan_term_yrs": 30,
+            "expected_repairs_monthly": 0.05,
+            "expected_vacancy_monthly": 0.09,
+            "expected_capx_monthly": 0.1,
+            "expected_management_monthly": 0.1,
+            "insurance_rate_yearly": 0.006,
+            "delete_excel_file": False,
+            "send_emails": False,
+        }
+        self.assertFalse(config_file_required_values_present(config))
 
     def test_empty_value(self):
         """Test case where one value is missing."""
